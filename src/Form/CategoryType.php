c92bb2bcc6b8962369f43bfb8c298e213861280e
Jordy GNANIH <jordygnanihpro@gmail.com>
2026-06-22 10:36:56 +0200

    link recipe to categorie

--- src/Form/CategoryType.php
+++ src/Form/CategoryType.php
@@ -3,6 +3,8 @@
 namespace App\Form;
 
 use App\Entity\Category;
+use App\Entity\Recipe;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Event\PreSubmitEvent;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
@@ -24,6 +26,13 @@ class CategoryType extends AbstractType
             ->add('slug', TextType::class, [
                 'empty_data' => "",
                 'required' => false
+            ])
+            ->add('recipes', EntityType::class, [
+                'class' => Recipe::class,
+                'choice_label' => 'title',
+                'multiple' => true,
+                'by_reference' => false,
+                'expanded' => true,
             ])
             ->add('Submit', SubmitType::class, [
                 'label' => 'Créer'
